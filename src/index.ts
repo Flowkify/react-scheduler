5230a7474995f8f56184a7a162d6d6cbc3bffb0c
wauters <jonaswauters@hotmail.be>
2025-09-06 21:34:36 +0200

    Expose TooltipData

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { Scheduler } from "./components";
 import "./styles.css";
 export type { SchedulerProps } from "./components/Scheduler/types";
-export type { SchedulerData, SchedulerProjectData, ZoomLevel, Config } from "./types/global";
+export type {
+  SchedulerData,
+  SchedulerProjectData,
+  ZoomLevel,
+  Config,
+  TooltipData
+} from "./types/global";
 
 export { Scheduler };
